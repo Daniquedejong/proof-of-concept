deaa9bb6a30366f0a2c2e1a36b320df8bb369412
Daniquedejong <danique.de.jong3@hva.nl>
2023-06-20 13:52:43 +0200

    help

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import "dotenv/config";
 
 // Express uit de nodemodules map
-import express, { json, response } from "express";
+
+import { render } from "ejs";
+import express, { response } from "express";
+import { format, formatISO, add, differenceInMinutes, differenceInHours } from "date-fns"
+import { utcToZonedTime } from "date-fns-tz"
+
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -13,20 +19,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async function (request, response) {
-  const employees = await dataFetch("https://api.werktijden.nl/2/employees")
+  const employees = await dataFetch("https://api.werktijden.nl/2/employees");
   console.log(employees);
-  response.render("aanwezigheid", {employees})
+  response.render("aanwezigheid", { employees });
 });
 
 const baseUrl = "https://api.werktijden.nl/2";
 
-const urlEmployees = `${baseUrl}/employees`;
+const url = `${baseUrl}/employees`;
 
-const urlPunches = `${baseUrl}/timeclock/punches`;
+
+// Haal de datum van vandaag op om alleen de punches van vandaag te laten zien:
+const date = new Date()
+const timeZone = 'Europe/Amsterdam'
+const zonedDate = utcToZonedTime(date, timeZone)
+const start = formatISO(new Date(zonedDate), { representation: 'date' })
+const end = formatISO(add(new Date(zonedDate), { days: 1 }), { representation: 'date' })
+
+const today = format(utcToZonedTime(date, timeZone), 'd-L-y')
+
+
+
+// Url voor ophalen alle clock ins/outs
+
+const punchesUrl = await dataFetch(
+  `https://api.werktijden.nl/2/timeclock/punches?departmentId=298538&start=${start}&end=${end}`
+);
 
 const urlClockin = `${baseUrl}/timeclock/clockin`;
 
 const urlCLockout = `${baseUrl}/timeclock/clockout`;
+
+
 
 const options = {
   method: "GET",
@@ -35,39 +59,154 @@ const options = {
   },
 };
 
+
+// GET-verzoek
+async function fetchData(url) {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+}
+
+// POST-verzoek
+async function postData(url, body) {
+  const postOptions = {
+    ...options,
+    method: "POST",
+    body: JSON.stringify(body)
+  };
+
+  const response = await fetch(url, postOptions);
+  const data = await response.json();
+  return data;
+}
+
+
+
+
+
+//route index
+// index
+app.get("/", async (request, response) => {
+  const employees = await dataFetch("https://api.werktijden.nl/2/employees");
+  const punches = await dataFetch(punchesUrl); // Fetch punches data
+
+
+
+  // console.log(employeesData);
+  // console.log(punchesData.data);
+
+  response.render("aanwezigheid", { employees, punches }); // Pass punches data to the template
+});
+
+//   response.render("aanwezigheid", { employee: employeesData, punches: punchesData.data ? punchesData.data : false });
+// });
+
+
+// // Fetch and render data
+// app.get("/", async (request, response) => {
+//   try {
+//     const employeesDataPromise = fetchData(urlEmployees);
+//     const punchesDataPromise = fetchData(punchesUrl);
+
+//     const [employeesData, punchesData] = await Promise.all([employeesDataPromise, punchesDataPromise]);
+
+//     response.render("aanwezigheid", { employee: employeesData, punches: punchesData.data || false });
+//   } catch (error) {
+//     console.error(error);
+//     response.status(500).send("Internal Server Error");
+//   }
+// });
+
+
+
+
+// GET-verzoek voor het ophalen van de recente uitkloktijd
+app.get("/clockout/:employeeId", async (request, response) => {
+  const { employeeId } = request.params;
+  const recentClockOutData = await fetchData(`/clockout/${employeeId}`);
+  const recentClockOutTime = recentClockOutData.timestamp;
+
+  response.send(recentClockOutTime);
+});
+
+//  GET-verzoek voor het ophalen van de recente inkloktijd
+app.get("/clockin/:employeeId", async (request, response) => {
+  const { employeeId } = request.params;
+  const recentClockInData = await fetchData(`/clockin/${employeeId}`);
+  const recentClockInTime = recentClockInData.timestamp;
+
+  response.send(recentClockInTime);
+});
+
+
+
+
+
+
+
+
+
+
 // post json
 
-// clockin
+// // clockin
 
-// voor het posten moet ik de employee_id en department_id(#departmentnummer)
+// // voor het posten moet ik de employee_id en department_id(#departmentnummer)
 
-export async function postClockinJson(UrlClockin, body) {
-  return await fetch(UrlClockin, {
-    method: "post",
+// export async function postClockinJson(UrlClockin, body) {
+//   return await fetch(UrlClockin, {
+//     method: "post",
 
-    body: JSON.stringify(body),
+//     body: JSON.stringify(body),
 
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((response) => response.json())
+//     headers: { "Content-Type": "application/json" },
+//   })
+//     .then((response) => response.json())
 
-    .catch((error) => error);
-}
+//     .catch((error) => error);
+// }
 
-// post de uitkloktijden
+// // post de uitkloktijden
 
-export async function postClockoutJson(UrlClockout, body) {
-  return await fetch(UrlClockout, {
-    method: "post",
+// export async function postClockoutJson(UrlClockout, body) {
+//   return await fetch(UrlClockout, {
+//     method: "post",
 
-    body: JSON.stringify(body),
+//     body: JSON.stringify(body),
 
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((response) => response.json())
+//     headers: { "Content-Type": "application/json" },
+//   })
+//     .then((response) => response.json())
 
-    .catch((error) => error);
-}
+//     .catch((error) => error);
+// }
+
+// Clock in
+app.post("/clockin", async (request, response) => {
+  const { employeeId, departmentId } = request.body;
+
+  const clockInData = await postData(clockinUrl, {
+    employee_id: employeeId,
+    department_id: departmentId
+  });
+    console.log(clockInData);
+    response.redirect("/");
+
+});
+
+// Clock out
+app.post("/clockout", async (request, response) => {
+  const { employeeId, departmentId } = request.body;
+
+  const clockOutData = await postData(clockoutUrl, {
+    employee_id: employeeId,
+    department_id: departmentId
+  });
+    console.log(clockOutData);
+    response.redirect("/");
+
+});
+
 
 
 
